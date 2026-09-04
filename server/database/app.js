f61473acce3dc7dfac1e98171c7e22812d207ffb
brandk94 <brandon.t.kennedy@comcast.net>
2026-09-03 23:55:43 -0400

    Implemented 3 new API endpoints for Review and Dealership MongoDB models.

--- server/database/app.js
+++ server/database/app.js
@@ -8,16 +8,19 @@ const port = 3030;
 app.use(cors())
 app.use(require('body-parser').urlencoded({ extended: false }));
 
+// Retrieve pre-written Review and Dealership data
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
 const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
+// Enact Mongoose ODM (Object Data Modeling) library to translate 
+// MongoDB schema-less data, injecting it into Node-based Express endpoints
 mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 
-
+// Import schema design files for Review and Dealership entries
 const Reviews = require('./review');
-
 const Dealerships = require('./dealership');
 
+// Load parsed into Review and Dealership model tables
 try {
   Reviews.deleteMany({}).then(()=>{
     Reviews.insertMany(reviews_data['reviews']);
@@ -58,17 +61,32 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const documents = await Dealerships.find();
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    try {
+        const documents = await Dealerships.find({ state: req.params.state });
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    try {
+        const documents = await Dealerships.find({ id: req.params.id });
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 //Express route to insert review
