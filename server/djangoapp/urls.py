9da8b93260b582095dac66d086fe598171c2d8e9
brandk94 <brandon.t.kennedy@comcast.net>
2026-09-04 23:17:35 -0400

    Edited restapis.py enabling HTTP requests from Django environment. Implemented sentiment analyzer method and new Django views for retrieve dealers and details.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,10 +15,18 @@ urlpatterns = [
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
-    # path for get_cars
+    # path for get cars
     path(route='get_cars', view=views.get_cars, name="getcars"),
 
+    # path for get dealers
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
+    # path for dealer details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+
     # path for dealer reviews view
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name="dealer_reviews"),
 
     # path for add a review view
 
